7b58a1a3e0b7d3bd86042ea0ad04f7c93a6c6f10
Aleksey Belchenkov <u608110@gmail.com>
2017-04-13 18:18:51 +0300

    Get Data from Server

--- js/main.js
+++ js/main.js
@@ -1,6 +1,31 @@
 $(() => {
 	$('#searchForm').on('submit', (e) => {
-		console.log($('#searchText').val());
-		e.preventDefault();
+		e.preventDefault();		
+		let searchText = $('#searchText').val();
+		getMovies(searchText);
+	
 	}); // on()
+
+	function getMovies(searchText) {
+		axios.get('http://www.omdbapi.com?s=' + searchText)
+			.then((response) => {
+				console.log(response);
+				let movies = response.data.Search;
+				let output = '';
+				$.each(movies, (index, movie) => {
+					output += `
+						<div class="col-md-3">
+				            <div class="well text-center">
+				              <img src="${movie.Poster}">
+				              <h5>${movie.Title}</h5>
+				              <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+				            </div>
+				        </div>
+					`;
+				});
+			})
+			.catch((error) => {
+				console.log(error);
+			})
+	}	
 });
